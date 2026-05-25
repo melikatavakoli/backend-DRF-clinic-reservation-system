6b4f128d98bd19af33d4cd94bf00272fe5a71d4f
melika <melikatavakoliedu@gmail.com>
2026-05-26 02:10:33 +0330

    feat: add permissions (not used yet)

---
+++ common/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+class IsDoctor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and hasattr(request.user, 'doctor_profile')
+
+class IsPatient(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and hasattr(request.user, 'patient_profile')
+
+class IsReceptionist(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and hasattr(request.user, 'receptionist_profile')
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_staff
